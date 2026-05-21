d1fe6dfe49e864f9d0e8005477114cca6c5e0b64
RapidMindd <YaroslavT007@yandex.ru>
2026-05-21 14:26:13 +0300

    comparator

---
+++ tarasenko.yaroslav/S4/binary_tree.hpp
@@ -0,0 +1,16 @@
+#ifndef BINARY_TREE_HPP
+#define BINARY_TREE_HPP
+
+#include <functional>
+#include "less_to.hpp"
+
+namespace tarasenko
+{
+  template< class Key, class Value, class Compare = LessTo< Key > >
+  class BSTree
+  {
+
+  };
+}
+
+#endif
