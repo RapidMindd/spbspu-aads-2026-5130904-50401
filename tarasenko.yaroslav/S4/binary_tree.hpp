f083a32c03b6cf655f62278255c34a7eb9cd12c6
RapidMindd <YaroslavT007@yandex.ru>
2026-06-04 13:22:51 +0300

    add and drop

--- tarasenko.yaroslav/S4/binary_tree.hpp
+++ tarasenko.yaroslav/S4/binary_tree.hpp
@@ -29,7 +29,7 @@ namespace tarasenko
   class BSTree
   {
   public:
-    void add(const Key& key, const Value& val);
+    bool add(const Key& key, const Value& val);
     bool drop(const Key& key);
     const Value& get(const Key& key) const;
     Value& get(const Key& key);
@@ -62,6 +62,11 @@ namespace tarasenko
     detail::Node< Key, Value >* root_;
     size_t size_;
     Compare comp_;
+
+  private:
+    detail::Node< Key, Value >* find(const Key& key) const;
+    void replace(detail::Node< Key, Value >* node, detail::Node< Key, Value >* child);
+    detail::Node< Key, Value >* siftLeft(detail::Node< Key, Value >* node);
   };
 
   template< class Key, class Value >
@@ -105,6 +110,145 @@ namespace tarasenko
     detail::Node< Key, Value >* node_ = nullptr;
     detail::Node< Key, Value >* root_ = nullptr;
   };
+
+  #define tree_template template< class Key, class Value, class Compare >
+  #define tree_type BSTree< Key, Value, Compare >
+  #define tree_iterator BSTIterator< Key, Value >
+  #define tree_const_iterator BSTConstIterator< Key, Value >
+  #define tree_node detail::Node< Key, Value >
+
+  tree_template
+  bool tree_type::add(const Key& key, const Value& val)
+  {
+    if (!root_)
+    {
+      root_ = new tree_node{{key, val}, nullptr, nullptr, nullptr};
+      ++size_;
+      return true;
+    }
+    tree_node* cur = root_;
+    while (cur)
+    {
+      if (comp_(cur->data_.first, key))
+      {
+        if (!cur->right_)
+        {
+          cur->right_ = new tree_node{{key, val}, nullptr, nullptr, cur};
+          ++size_;
+          return true;
+        }
+        cur = cur->right_;
+      }
+      else if (comp_(key, cur->data_.first))
+      {
+        if (!cur->left_)
+        {
+          cur->left_ = new tree_node{{key, val}, nullptr, nullptr, cur};
+          ++size_;
+          return true;
+        }
+        cur = cur->left_;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    return false;
+  }
+
+  tree_template
+  tree_node* tree_type::find(const Key& key) const
+  {
+    tree_node* cur = root_;
+    while (cur)
+    {
+      if (comp_(cur->data_.first, key))
+      {
+        cur = cur->right_;
+      }
+      else if (comp_(key, cur->data_.first))
+      {
+        cur = cur->left_;
+      }
+      else
+      {
+        return cur;
+      }
+    }
+    return nullptr;
+  }
+
+  tree_template
+  void tree_type::replace(tree_node* node, tree_node* child)
+  {
+    if (node->parent_ == nullptr)
+    {
+      root_ = child;
+    }
+    else if (node == node->parent->left)
+    {
+      node->parent_->left_ = child;
+    }
+    else
+    {
+      node->parent_->right_ = child;
+    }
+    if (child)
+    {
+      child->parent_ = node->parent_;
+    }
+  }
+
+  tree_template
+  tree_node* tree_type::siftLeft(tree_node* node)
+  {
+    while (node->left_)
+    {
+      node = node->left_;
+    }
+    return node;
+  }
+
+  tree_template
+  bool tree_type::drop(const Key& key)
+  {
+    tree_node* node = find(key);
+    if (!node)
+    {
+      return false;
+    }
+    if (!node->left_)
+    {
+      replace(node, node->right_);
+      delete node;
+      return true;
+    }
+    if (!node->right_)
+    {
+      replace(node, node->left_);
+      delete node;
+      return true;
+    }
+    tree_node* next = siftLeft(node->right_);
+    if (next->parent_ != node)
+    {
+      replace(next, next->right_);
+      next->right_ = node->right_;
+      next->right_->parent_ = next;
+    }
+    replace(node, next);
+    next->left_ = node->left_;
+    next->left_->parent_ = next;
+    delete node;
+    return true;
+  }
+
+  #undef tree_template
+  #undef tree_type
+  #undef tree_iterator
+  #undef tree_const_iterator
+  #undef tree_node
 }
 
 #endif
